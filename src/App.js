dae09cfc8b40103adbb93b1f8cf8767ea3fa4986
lnyberga <linda.nybergandersson@hotmail.com>
2021-11-17 20:47:38 +0100

    Gör en Branch för att bygga views.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import Display from "./christian/Display";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Grupp 3 - Arbete</h1>
-        <Display />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bygger views</h1>
       </header>
     </div>
   );
