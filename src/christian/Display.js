f9463ea9b81a6abda8814768da7e277ed0305849
Christian Areschoug <areschoug@gmail.com>
2021-11-19 12:05:32 +0100

    Grup 3s komp
    testar

--- src/christian/Display.js
+++ src/christian/Display.js
@@ -3,7 +3,7 @@ import React from "react";
 function Display() {
   return (
     <div>
-      <h2 className="display-4 display">Christians Komponent</h2>
+      <h2 className="display-4 display">Grupp 3s Komponent</h2>
     </div>
   );
 }
