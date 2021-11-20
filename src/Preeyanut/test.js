10b0d915cfa5dcff767522b4f6178ece6eb2a404
preeyaJo <58887622+preeyaJo@users.noreply.github.com>
2021-11-20 11:56:54 +0100

    Create test.js
    laddar upp test

---
+++ src/Preeyanut/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+function test() {
+    return (
+      <div>
+        <h1>Preeyanut testar</h1>      
+      </div>
+    );
+  }
+
+export default test;
