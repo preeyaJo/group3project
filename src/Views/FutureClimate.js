57870d4b1f65e8d378197e74195ca40ce592dead
idajosefin99 <ida_laarsson@hotmail.se>
2021-11-23 09:28:50 +0100

    Fixat timeline samt active i bottenmenyn
    Fixat timeline samt active i bottenmenyn

--- src/Views/FutureClimate.js
+++ src/Views/FutureClimate.js
@@ -1,18 +1,28 @@
 import React from "react";
-import "../App.css";
+import "../futureclimate.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "react-bootstrap/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleRight, faLongArrowAltDown} from "@fortawesome/free-solid-svg-icons";
+
 
 import { Link } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
+import FutureListItems from "../Components/FutureListItems";
 
 export function FutureClimate() {
   return (
     <div>
-      <h1>Vår framtid</h1>
-      <Link to="/fossilfuels">
-        <Button>Så löser vi det</Button>
+      <Navbar className="BottomMenu" fixed="top">
+            <h1 className="text-center">Vår framtid <FontAwesomeIcon icon={faLongArrowAltDown}></FontAwesomeIcon></h1>
+            </Navbar>
+      <FutureListItems/>
+      <Navbar className="BottomMenu" fixed="bottom">
+      <Link className="button-to-cctopics" to="/fossilfuels">
+        <a>Så löser vi det  <FontAwesomeIcon icon={faArrowCircleRight}></FontAwesomeIcon></a>
       </Link>
+            </Navbar>
     </div>
   );
 }
