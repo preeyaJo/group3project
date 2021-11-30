b473c3b8c5533a7fc16e6a56147cbb91ab2a74a4
idajosefin99 <ida_laarsson@hotmail.se>
2021-11-30 08:59:00 +0100

    Timeline fix - ej löst problemet med mellanrum längst upp och ner
    Timeline fix - ej löst problemet med mellanrum längst upp och ner

--- src/Views/FutureClimate.js
+++ src/Views/FutureClimate.js
@@ -13,13 +13,13 @@ import FutureListItems from "../Components/FutureListItems";
 export function FutureClimate() {
   return (
     <div>
-      <Navbar className="BottomMenu" fixed="top"> {/*Toppmeny*/}
-            <h1 className="text-center">Vår framtid <FontAwesomeIcon icon={faLongArrowAltDown}></FontAwesomeIcon></h1>
+      <Navbar className="BottomMenu justify-content-center" fixed="top"> {/*Toppmeny*/}
+            <h1 className="">Vår framtid <FontAwesomeIcon icon={faLongArrowAltDown}></FontAwesomeIcon></h1>
             </Navbar>
-            
+            <section className="content-futureclimate">
       <FutureListItems/>{/*Timeline*/}
-
-      <Navbar className="BottomMenu" fixed="bottom"> {/*Bottenmeny*/}
+            </section>
+      <Navbar className="BottomMenu justify-content-center" fixed="bottom"> {/*Bottenmeny*/}
       <Link className="button-to-cctopics" to="/fossilfuels"> {/*Knapp til CCTopics*/}
         Så löser vi det  <FontAwesomeIcon icon={faArrowCircleRight}></FontAwesomeIcon>
       </Link>
