6e802bcfd6a14d4cfd1bd8348ff08716c260c55e
lnyberga <linda.nybergandersson@hotmail.com>
2021-12-07 13:17:48 +0100

    <Inlog Function>

--- src/Components/Inlog.js
+++ src/Components/Inlog.js
@@ -1,39 +1,83 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+/* Import State from React */
+import React, { useState } from "react";
 
+/* Import style */
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Link } from 'react-router-dom';
+/* Import icons */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobeEurope } from "@fortawesome/free-solid-svg-icons";
 
-
-import Button from 'react-bootstrap/Button';
+/* Import Bootstrap */
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
 
-export function Inlog() {
+
+/* Start function for inlogForm */
+export function Inlog({Login, error}) {
+  const [details, setDetails] = useState({name: "", email: "", password: ""});
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    Login(details);
+  }
   return (
     <div>
-    
+  
+  <h1 className="startHeader">Lär för framtiden</h1>
+      <FontAwesomeIcon
+        icon={faGlobeEurope}
+        className="startLogga"
+      ></FontAwesomeIcon>
+
+        {(error !== "") ? ( <div className="error">{error}</div> ) : ""}
+
         <div className="loginForm mb-3">
         <Stack gap={2} className="col-7 mx-auto">
-        <Form>
+        <Form onSubmit={submitHandler}>
+
+
             <Form.Group className="mb-2" controlId="formGroupEmail">
                 <Form.Label>Id:</Form.Label>
-                <Form.Control type="email" placeholder="FörnamnEfternamn123" />
+                <Form.Control type="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} placeholder="Exemel: Efternamn123" />
             </Form.Group>
+
+
             <Form.Group className="mb-3" controlId="formGroupPassword">
                 <Form.Label>Lösenord:</Form.Label>
-                <Form.Control type="password" placeholder="Ditt lösenord"/>
+                <Form.Control type="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} placeholder="Ditt lösenord"/>
             </Form.Group>
+
+            
+            
+            {/* Start button */}
+              <input type="submit" value="Starta" />
+           
+
         </Form>
         </Stack>
 
         </div>
 
-        
+      {/*  
       <Link to="/futureclimate">
-        <Button className="startButton mb-5" >Starta</Button>
-      </Link>
+        <Button className="startButton mb-5" type="submit" value="login">Starta</Button>
+      </Link>*/}
+
+<div className="group3">
+        <h2>En app av Grupp 3</h2>
+        <div className="groupNames3">
+          <p>
+            Linda Nyberg Andersson <br></br>
+            Christian Areschoug <br></br>
+            Elin Ljungquist <br></br>
+            Preeyanut Olin Joomkudrang <br></br>
+            Ida Larsson <br></br>
+          </p>
+        </div>
+      </div>
       
     </div>
   );
--- src/Views/Start.js
+++ src/Views/Start.js
@@ -1,39 +1,70 @@
-import React from "react";
+/* Import state from react */
+import React, {useState} from "react";
+
+/* Import style */
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobeEurope } from "@fortawesome/free-solid-svg-icons";
+/* Import React Router Dom */
+import { Redirect } from "react-router-dom";
 
-/* 
-import Button from 'react-bootstrap/Button'; */
-
+/* Import components */
 import Inlog from "../Components/Inlog";
 
+
+
+/* Start function for inlog */
 export function Start() {
+
+  /***** Script area *****/
+
+  /* Set data to user */
+  const adminUser = {
+    name: "Anna",
+    email: "anna@anna.com",
+    password: "anna123"
+  }
+
+  /* State to login */
+  const [user, setUser] = useState({email: ""});
+  const [error, setError] = useState("");
+
+  /* Function for login in */
+  const Login = details => {
+
+    /* Checks if the user input data is correct to adminUser */
+    if (details.email === adminUser.email && details.password === adminUser.password){
+
+      /* set details to state */
+      setUser({
+        email: details.email
+      });
+      
+      
+    /* If the user data is incorrect, try again */  
+    } else {
+      setError("Felaktiga inloggningsuppgifter");
+      
+    }
+  }
+
+  /***** Output area *****/
+
   return (
     <div>
-      <h1 className="startHeader">Lär för framtiden</h1>
-      <FontAwesomeIcon
-        icon={faGlobeEurope}
-        className="startLogga"
-      ></FontAwesomeIcon>
 
-      <Inlog />
-      <div className="group3">
-        <h2>En app av Grupp 3</h2>
-        <div className="groupNames3">
-          <p>
-            Linda Nyberg Andersson <br></br>
-            Christian Areschoug <br></br>
-            Elin Ljungquist <br></br>
-            Preeyanut Olin Joomkudrang <br></br>
-            Ida Larsson <br></br>
-          </p>
-        </div>
+      {/* Change path when inlog data is correct */}
+      {(user.email !== "") ? (
+        <div>
+        <Redirect to="/futureclimate"></Redirect>
       </div>
+      ) : (
+        <Inlog Login={Login} error={error}/>
+      )}
+      
     </div>
   );
 }
 
+/***** Send component content *****/
 export default Start;
