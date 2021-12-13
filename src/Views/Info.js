1df04a5f23d76f0e18a0a57a590971909fb40049
elinjohannadaniella <94492047+elinjohannadaniella@users.noreply.github.com>
2021-12-13 16:13:00 +0100

    fiiixi

--- src/Views/Info.js
+++ src/Views/Info.js
@@ -1,11 +1,9 @@
 
 import React from "react";
 
-
-
 /* Import internal sources */
 import "../Style/App.css";
-import "../Info.css";
+import "../Style/Info.css";
 import GlobalHeader from "../Components/GlobalHeader";
 import BottomMenu from "../Components/BottomMenu";
 
@@ -17,33 +15,51 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-
-
-
 export function Information() {
   
   return (
     <div>
+      {/* retreving globalheader*/}
       <GlobalHeader />
       <div className="container extraSpacing">
-      <div className="m-3 infoText">
-      <h2>Information till lärare och vårdnashavare</h2>
-      <p>Vi på Climate Change har på uppdrag av skolverket fått i uppgift att lära ungdomar i åk 7-9 i Sverige om klimatet och dess påverkan på vår jord på ett strukturerat sätt med hjälp av kommunala skolor runt om i landet. Vårt mål är att i tidig ålder få barn och ungdomar att förstå hotet med att utnyttja jordens resurser i en sådan hastighet att det blir livshotande för oss människor men även den biologiska mångfalden i naturen.</p>
-      <p>Genom vår webbsida vill vi förmedla kunskap om den globala uppvärmingen och dess konsekvenser men även ge tips till barnen/ungdomarna om vad dem kan göra för att påverka den globalauppvärmingen i mindre utsträckning.</p>
+        <div className="m-3 infoText">
+          <h2>Information till lärare och vårdnashavare</h2>
+          <p>
+            Vi på Climate Change har på uppdrag av skolverket fått i uppgift att
+            lära ungdomar i åk 7-9 i Sverige om klimatet och dess påverkan på
+            vår jord på ett strukturerat sätt med hjälp av kommunala skolor runt
+            om i landet. Vårt mål är att i tidig ålder få barn och ungdomar att
+            förstå hotet med att utnyttja jordens resurser i en sådan hastighet
+            att det blir livshotande för oss människor men även den biologiska
+            mångfalden i naturen.
+          </p>
+          <p>
+            Genom vår webbsida vill vi förmedla kunskap om den globala
+            uppvärmingen och dess konsekvenser men även ge tips till
+            barnen/ungdomarna om vad dem kan göra för att påverka den
+            globalauppvärmingen i mindre utsträckning.
+          </p>
 
-      <div className="infoContact">
-      <h3>Här ni några frågor, tveka inte att kontakta oss</h3>
-      <div><FontAwesomeIcon icon={faPhone}/> <a href="tel:000-0000000">010-10 100 10</a></div>
-      <address><FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:climatechange@change.se">climatechange@change.se</a></address>
+          <div className="infoContact">
+            <h3>Här ni några frågor, tveka inte att kontakta oss</h3>
+            <div>
+              {/* fetching icons */}
+              <FontAwesomeIcon icon={faPhone} />{" "}
+              <a href="tel:000-0000000">010-10 100 10</a>
+            </div>
+            <address>
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+              <a href="mailto:climatechange@change.se">
+                climatechange@change.se
+              </a>
+            </address>
+          </div>
+        </div>
       </div>
-      </div>
-      
-      </div>
+      {/* retreving bottom menu */}
       <BottomMenu />
     </div>
   );
 }
-
-
 
 export default Information;
